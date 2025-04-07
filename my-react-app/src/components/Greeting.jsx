b64dce150bf8e5517180b98f87c209ffed0107ef
Divya Joshi <divyaj.redj@gmail.com>
2025-04-06 22:03:39 -0400

    Project Setup

---
+++ my-react-app/src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Greeting() { // This is a functional component
+    return <div>
+        <hr/>
+        <h2>Hello from the Greeting Component!</h2>
+        <p>This is your first custom component in React.</p>
+    </div>;
+}
+export default Greeting;
